c6ec185496e63e99768879b549ba688a6ea42346
Will Tomlins <will@tomlins.org.uk>
2014-07-22 11:55:25 +0100

    Expose govuk_admin_template styleguide
    at /styleguide when in development mode

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "dashboard#dashboard"
 
   get "/healthcheck" => Proc.new { [200, {}, ["OK"]] }
+
+  if Rails.env.development?
+    get "/styleguide" => "govuk_admin_template/style_guide#index"
+  end
 end
