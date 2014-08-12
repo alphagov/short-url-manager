9faad1066af17f3c884251af9d33f040eec648e6
Will Tomlins <will@tomlins.org.uk>
2014-08-12 13:24:56 +0100

    Use factories in FurlRequest spec

--- spec/models/furl_request_spec.rb
+++ spec/models/furl_request_spec.rb
@@ -1,76 +1,33 @@
 require 'rails_helper'
 
 describe FurlRequest do
-  let(:instance) { FurlRequest.new(attributes) }
-  let(:attributes) { {
-    from: from,
-    to: to,
-    reason: reason,
-    contact_email: contact_email,
-    organisation_slug: organisation_slug,
-    organisation_title: organisation_title
-  } }
-  let(:from) { "/some-friendly-url" }
-  let(:to) { "/government/some-publication" }
-  let(:reason) { "Some reason" }
-  let(:contact_email) { "someone@example.com" }
-  let(:organisation_slug) { 'organisation-slug' }
-  let(:organisation_title) { 'Organisation Title' }
-
   describe "validations:" do
-    specify { expect(instance).to be_valid }
+    specify { expect(build :furl_request).to be_valid }
+    specify { expect(build :furl_request, from: '').to_not be_valid }
+    specify { expect(build :furl_request, to: '').to_not be_valid }
+    specify { expect(build :furl_request, reason: '').to_not be_valid }
+    specify { expect(build :furl_request, contact_email: '').to_not be_valid }
+    specify { expect(build :furl_request, contact_email: 'invalid.email.address').to_not be_valid }
+    specify { expect(build :furl_request, organisation_title: '').to_not be_valid }
+    specify { expect(build :furl_request, organisation_slug: '').to_not be_valid }
 
-    context "without 'from'" do
-      let(:from) { '' }
-      specify { expect(instance).to_not be_valid }
+    it "should be invalid when from is not a relative path" do
+      expect(build :furl_request, from: 'http://www.somewhere.com/a-path').to_not be_valid
     end
-
-    context "when 'from' is present, but is not a relative path" do
-      let(:from) { 'http://www.somewhere.com/a-path' }
-      specify { expect(instance).to_not be_valid }
+    it "should be invalid when to is not a relative path" do
+      expect(build :furl_request, to: 'http://www.somewhere.com/a-path').to_not be_valid
     end
-
-    context "without 'to'" do
-      let(:to) { '' }
-      specify { expect(instance).to_not be_valid }
-    end
-
-    context "when 'to' is present, but is not a relative path" do
-      let(:to) { 'http://www.somewhere.com/a-path' }
-      specify { expect(instance).to_not be_valid }
-    end
-
-    context "without 'reason'" do
-      let(:reason) { '' }
-      specify { expect(instance).to_not be_valid }
-    end
-
-    context "without 'contact_email'" do
-      let(:contact_email) { '' }
-      specify { expect(instance).to_not be_valid }
-    end
-
-    context "when 'contact_email' is present, but not a valid email address" do
-      let(:contact_email) { 'invalid.email.address' }
-      specify { expect(instance).to_not be_valid }
-    end
-
-    context "without 'organisation_slug'" do
-      let(:organisation_slug) { '' }
-      specify { expect(instance).to_not be_valid }
-    end
-
-    context "without 'organisation_title'" do
-      let(:organisation_title) { '' }
-      specify { expect(instance).to_not be_valid }
+    it "should be invalid when contact_email is not a valid email address" do
+      expect(build :furl_request, contact_email: 'invalid.email.address').to_not be_valid
     end
   end
 
   describe "organisation fields" do
     context "when an organisation slug for an existing organisation is given" do
       let!(:organisation) { create :organisation }
-      let(:organisation_slug) { organisation.slug }
-      let(:organisation_title) { nil }
+      let(:instance) { build :furl_request, organisation_slug: organisation.slug,
+                                            organisation_title: organisation.title
+      }
 
       it "should set organisation_title to that of the organisation before validating" do
         expect(instance).to be_valid
