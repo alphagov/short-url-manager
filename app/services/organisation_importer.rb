a7ffd4638d38b37cd348532f903d46d53a9725fb
Will Tomlins <will@tomlins.org.uk>
2014-09-09 17:02:41 +0100

    Add locking to OrganisationImporter
    The organisation importer cron job runs on all instances of the application,
    which apart from doing work unnecessarily, was also sometimes generating
    duplicate organisations.
    
    Use redis as a locking mechanism so only the first process actually
    does the import.

--- app/services/organisation_importer.rb
+++ app/services/organisation_importer.rb
@@ -1,4 +1,6 @@
 require 'gds_api/organisations'
+require 'redis'
+require 'redis-lock'
 
 class OrganisationImporter
   attr_reader :api_url_base
@@ -8,11 +10,15 @@ class OrganisationImporter
   end
 
   def perform!
-    organisations_data = get_organisations_data
-    Organisation.destroy_all
-    organisations_data.each {|attrs|
-      Organisation.create(attrs)
-    }
+    redis.lock("short_url_manager:#{Rails.env}:organisation_importer_lock", :life => 2.hours) do
+      organisations_data = get_organisations_data
+      Organisation.destroy_all
+      organisations_data.each {|attrs|
+        Organisation.create(attrs)
+      }
+    end
+  rescue Redis::Lock::LockNotAcquired => e
+    Rails.logger.warn("Failed to get lock for importing organisations (#{e.message}). Another process probably got there first.")
   end
 
 private
@@ -27,5 +33,10 @@ private
 
   def api_adapter
     @api_adapter ||= GdsApi::Organisations.new(api_url_base)
+  end
+
+  def redis
+    redis_config = YAML.load_file(Rails.root.join("config", "redis.yml"))
+    Redis.new(redis_config.symbolize_keys)
   end
 end
