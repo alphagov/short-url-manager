f2d2f59d1e4ffe23c96156ca1ec12f967df14a25
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-09-07 16:36:30 +0000

    Add breadcrumbs and tweak buttons
    Remove now redundant Back buttons. Add spacing to avoid accidental clicks.

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,43 @@
+crumb :root do
+  link "Dashboard", root_path
+end
+
+crumb :new_short_url_request do
+  link "Request a short URL", new_short_url_request_path
+  parent :root
+end
+
+crumb :short_url_requests do
+  link "Short URL requests", short_url_requests_path
+  parent :root
+end
+
+crumb :live_short_urls do
+  link "Live short URLs", list_short_urls_path
+  parent :root
+end
+
+crumb :short_url_request do |short_url_request|
+  link "View short URL request", short_url_request_path(short_url_request)
+  parent :short_url_requests
+end
+
+crumb :edit_short_url_request do |short_url_request|
+  link "Edit short URL", edit_short_url_request_path(short_url_request)
+  parent :short_url_request, short_url_request
+end
+
+crumb :reject_short_url_request do |short_url_request|
+  link "Reject short URL", reject_short_url_request_path(short_url_request)
+  parent :short_url_request, short_url_request
+end
+
+crumb :short_url_request_accepted do |short_url_request|
+  link "Short URL request accepted"
+  parent :short_url_request, short_url_request
+end
+
+crumb :short_url_request_accepted_failed do |short_url_request|
+  link "Short URL request accept failed"
+  parent :short_url_request, short_url_request
+end
