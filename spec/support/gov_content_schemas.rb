c57891b27b3a4f0ac70540ba06f0f3fadff17db3
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-05-25 14:41:30 +0000

    Validate against v2 schemas.

--- spec/support/gov_content_schemas.rb
+++ spec/support/gov_content_schemas.rb
@@ -4,6 +4,6 @@ require 'govuk-content-schema-test-helpers/rspec_matchers'
 RSpec.configuration.include GovukContentSchemaTestHelpers::RSpecMatchers
 
 GovukContentSchemaTestHelpers.configure do |config|
-  config.schema_type = 'publisher'
+  config.schema_type = 'publisher_v2'
   config.project_root = Rails.root
 end
