ca27b3c12dbb2a98a6555b007bb413595e3fe898
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2016-09-30 11:10:22 +0100

    Require gov.uk campaign URLs to be https

--- spec/support/shared_examples_for_short_url_validations.rb
+++ spec/support/shared_examples_for_short_url_validations.rb
@@ -20,15 +20,15 @@ shared_examples_for "ShortUrlValidations" do
     end
 
     it "may be a gov.uk campaign URL" do
-      expect(build :short_url_request, to_path: 'http://my.campaign.gov.uk').to be_valid
       expect(build :short_url_request, to_path: 'https://my.campaign.gov.uk').to be_valid
-      expect(build :short_url_request, to_path: 'http://my.campaign.gov.uk/path').to be_valid
       expect(build :short_url_request, to_path: 'https://my.campaign.gov.uk/path').to be_valid
     end
 
     it "must be either a relative path or a gov.uk campaign URL" do
-      expect(build :short_url_request, to_path: 'http://www.somewhere.com/a-path').to_not be_valid
-      expect(build :short_url_request, to_path: 'http://.campaign.gov.uk').to_not be_valid
+      expect(build :short_url_request, to_path: 'https://www.somewhere.com/a-path').to_not be_valid
+      expect(build :short_url_request, to_path: 'https://.campaign.gov.uk').to_not be_valid
+      expect(build :short_url_request, to_path: 'http://my.campaign.gov.uk').to_not be_valid
+      expect(build :short_url_request, to_path: 'http://my.campaign.gov.uk/path').to_not be_valid
       expect(build :short_url_request, to_path: 'ftp://my.campaign.gov.uk').to_not be_valid
     end
   end
