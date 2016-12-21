0b2f3bb6284f328d059dbf0a050cb157afaa3256
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2016-12-21 15:48:06 +0000

    Change autoload_paths to eager_load_paths
    When Rails 5 runs in production it does not autoload files any more.
    Any files that are not included within config.eager_load_paths and are
    accessed will result in a `NameError: uninitialized constant`.

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ Bundler.require(*Rails.groups)
 
 module ShortUrlManager
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/app #{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/app #{config.root}/lib)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
