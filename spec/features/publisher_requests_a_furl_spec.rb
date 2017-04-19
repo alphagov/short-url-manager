3224ac8f1aab7e57fec852e8d12bb6932048e1d0
Kelvin Gan <dev@kelvingan.co.uk>
2017-04-19 16:42:13 +0100

    Amend copy and options text for segments
    As per suggestions from Ben Hazell and Joe Lanman, this is a simple
    text change for now. Further iteration of the UI is required.

--- spec/features/publisher_requests_a_furl_spec.rb
+++ spec/features/publisher_requests_a_furl_spec.rb
@@ -17,7 +17,7 @@ feature "As a publisher, I can request a short URL" do
     fill_in "From or short URL",  with: from_path = "/some-friendly-url"
     fill_in "Target URL",         with: to_path = "/government/publications/some-random-publication"
     select "Exact URL only",      from: "short_url_request_route_type"
-    select "ignore",              from: "Segments mode"
+    select "Discard",             from: "short_url_request_segments_mode"
     select "Ministry of Beards",  from: "Organisation"
     fill_in "Reason",             with: reason = "Because of the wombats"
 
