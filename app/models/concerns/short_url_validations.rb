de1cb7a8f5c035ba797195f227493fca49b6e823
Kelvin Gan <dev@kelvingan.co.uk>
2017-05-31 15:40:44 +0100

    Revert "Validate any GOV.UK subdomain"
    This reverts commit 5ec8f43a0c805591326aff138184f746e4e5db3b.

--- app/models/concerns/short_url_validations.rb
+++ app/models/concerns/short_url_validations.rb
@@ -8,14 +8,14 @@ module ShortUrlValidations
   end
 
   def to_path_is_valid
-    unless to_path.blank? || to_path =~ /\A\// || govuk_subdomain_url?(to_path)
+    unless to_path.blank? || to_path =~ /\A\// || govuk_campaign_url?(to_path)
       errors.add(:to_path, 'must be a relative path (eg. "/hmrc/tax-returns") or a gov.uk campaign URL (eg. "https://my-campaign-title.campaign.gov.uk/an-optional-path")')
     end
   end
 
-  def govuk_subdomain_url?(path)
+  def govuk_campaign_url?(path)
     uri = URI.parse(path)
-    uri.host =~ /\A([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[A-Za-z0-9])?\.)*gov\.uk\z/i && uri.scheme == 'https'
+    uri.host =~ /\A.+\.campaign\.gov\.uk\z/i && uri.scheme == 'https'
   rescue
     false
   end
