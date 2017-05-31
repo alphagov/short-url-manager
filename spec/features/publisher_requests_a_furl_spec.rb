100a5723b99545a029b95ba587ee460a07589a1a
Kelvin Gan <dev@kelvingan.co.uk>
2017-05-31 15:19:11 +0100

    Revert "Add route type and segments mode"

--- spec/features/publisher_requests_a_furl_spec.rb
+++ spec/features/publisher_requests_a_furl_spec.rb
@@ -16,8 +16,6 @@ feature "As a publisher, I can request a short URL" do
 
     fill_in "From or short URL",  with: from_path = "/some-friendly-url"
     fill_in "Target URL",         with: to_path = "/government/publications/some-random-publication"
-    select "exact",               from: "Route type"
-    select "ignore",              from: "Segments mode"
     select "Ministry of Beards",  from: "Organisation"
     fill_in "Reason",             with: reason = "Because of the wombats"
 
