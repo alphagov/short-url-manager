5bcb722cd2cd96465f8fb61c526378b2e85d1a03
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-03-12 12:59:25 +0000

    Remove HTML views
    We don't need them now that we're sending emails using Notify.

--- spec/support/email_matchers.rb
+++ spec/support/email_matchers.rb
@@ -4,27 +4,19 @@ RSpec::Matchers.define :have_body_content do |expected_text|
   # Use this matcher to test whether an email has body text in both the HTML and text mail parts.
 
   match do |sent_mail|
-    html_part, text_part = get_mail_body_parts(sent_mail)
-
-    html_part.include?(expected_text) && text_part.include?(expected_text)
+    text_part = get_mail_body(sent_mail)
+    text_part.include?(expected_text)
   end
 
   match_when_negated do |sent_mail|
-    html_part, text_part = get_mail_body_parts(sent_mail)
-
-    !html_part.include?(expected_text) && !text_part.include?(expected_text)
+    text_part = get_mail_body(sent_mail)
+    !text_part.include?(expected_text)
   end
 
   failure_message do |sent_mail|
-    html_part, text_part = get_mail_body_parts(sent_mail)
+    text_part = get_mail_body(sent_mail)
 
     message = "Expected the email to include \"#{expected_text}\".\n"
-    if !html_part.include?(expected_text)
-      message << "The HTML part did not include \"#{expected_text}\". HTML content: \n"
-      message << "-------------------------------------------------------------\n"
-      message << "#{html_part}\n"
-      message << "-------------------------------------------------------------\n\n"
-    end
     if !text_part.include?(expected_text)
       message << "The text part did not include \"#{expected_text}\". Text content: \n"
       message << "-------------------------------------------------------------\n"
@@ -35,15 +27,9 @@ RSpec::Matchers.define :have_body_content do |expected_text|
   end
 
   failure_message_when_negated do |sent_mail|
-    html_part, text_part = get_mail_body_parts(sent_mail)
+    text_part = get_mail_body(sent_mail)
 
     message = "Expected the email not to include \"#{expected_text}\".\n"
-    if html_part.include?(expected_text)
-      message << "The HTML part included \"#{expected_text}\". HTML content: \n"
-      message << "-------------------------------------------------------------\n"
-      message << "#{html_part}\n"
-      message << "-------------------------------------------------------------\n\n"
-    end
     if text_part.include?(expected_text)
       message << "The text part included \"#{expected_text}\". Text content: \n"
       message << "-------------------------------------------------------------\n"
@@ -53,10 +39,8 @@ RSpec::Matchers.define :have_body_content do |expected_text|
     message
   end
 
-  def get_mail_body_parts(sent_mail)
-    html_part = compact_content Nokogiri::HTML.parse(sent_mail.html_part.decoded).content
-    text_part = compact_content sent_mail.text_part.decoded
-    [html_part, text_part]
+  def get_mail_body(sent_mail)
+    compact_content sent_mail.body.decoded
   end
 
   def compact_content(raw_content)
