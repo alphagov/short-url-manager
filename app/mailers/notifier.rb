057be805fe14d69822debdbde96392147765e70d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-03-12 13:01:34 +0000

    Remove send_mail common method
    `view_mail` uses the underlying `mail` method from ActionMailer, which
    works out which template to use based on the name of the method that
    called it.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,29 +2,27 @@ class Notifier < ApplicationMailer
   add_template_helper UrlHelper
   default from: '"Short URL manager" <noreply+short-url-manager@digital.cabinet-office.gov.uk>'
 
-  def short_url_requested(short_url_request, recipients)
+  def short_url_requested(short_url_request, recipient)
+    @short_url_request = short_url_request
     subject = "#{prefix}Short URL request for '#{short_url_request.from_path}' by #{short_url_request.organisation_title}"
-    send_mail(recipients, subject, short_url_request)
+    view_mail(template_id, to: recipient, subject: subject)
   end
 
-  def short_url_request_accepted(short_url_request, recipients)
+  def short_url_request_accepted(short_url_request, recipient)
+    @short_url_request = short_url_request
     subject = "#{prefix}Short URL request approved"
-    send_mail(recipients, subject, short_url_request)
+    view_mail(template_id, to: recipient, subject: subject)
   end
 
-  def short_url_request_rejected(short_url_request, recipients)
+  def short_url_request_rejected(short_url_request, recipient)
+    @short_url_request = short_url_request
     subject = "#{prefix}Short URL request denied"
-    send_mail(recipients, subject, short_url_request)
+    view_mail(template_id, to: recipient, subject: subject)
   end
 
 private
 
   attr_reader :short_url_request
-
-  def send_mail(to, subject, short_url_request)
-    @short_url_request = short_url_request
-    view_mail(template_id, to: to, subject: subject)
-  end
 
   def template_id
     @template_id ||= ENV.fetch("GOVUK_NOTIFY_TEMPLATE_ID", "fake-test-template-id")
