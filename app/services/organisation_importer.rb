7623dad2de7cbc4ad302aec1ab88ee843dd6f5dd
Kevin Dew <kevindew@me.com>
2021-01-14 22:21:28 +0000

    Remove need for REDIS_HOST and REDIS_PORT env vars
    This is one of the few GOV.UK projects that uses this old approach to
    configuring Redis - whereas most of the other ones use the approach
    supported natively by the Redis gem [1] of using REDIS_URL. Removing
    this will allow us to remove the need for REDIS_HOST and REDIS_PORT env
    vars in our architecture and also allows this code to be simplified.
    
    [1]: https://github.com/redis/redis-rb

--- app/services/organisation_importer.rb
+++ app/services/organisation_importer.rb
@@ -10,7 +10,7 @@ class OrganisationImporter
   end
 
   def perform!
-    redis.lock("short_url_manager:#{Rails.env}:organisation_importer_lock", life: 2.hours) do
+    Redis.current.lock("short_url_manager:#{Rails.env}:organisation_importer_lock", life: 2.hours) do
       organisations_data = get_organisations_data
       Organisation.destroy_all
       organisations_data.each do |attrs|
@@ -34,10 +34,5 @@ private
 
   def api_adapter
     @api_adapter ||= GdsApi::Organisations.new(api_url_base)
-  end
-
-  def redis
-    redis_config = Rails.application.config_for(:redis)
-    Redis.new(redis_config.symbolize_keys)
   end
 end
