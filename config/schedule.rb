c1092d011282d1569667482a02ce83fc89e86f5c
martynipratt <martyn.pratt@digital.cabinet-office.gov.uk>
2023-03-23 10:28:38 +0000

    Require empty config/schedule.rb for cron job removal

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,1 @@
-# default cron env is "/usr/bin:/bin" which is not sufficient as govuk_env is in /usr/local/bin
-env :PATH, "/usr/local/bin:/usr/bin:/bin"
-
-# We need Rake to use our own environment
-job_type :rake, "cd :path && govuk_setenv short-url-manager bundle exec rake :task :output"
-
-set :output, error: "log/cron.error.log", standard: "log/cron.log"
-
-every :day, at: "4am" do
-  rake "organisations:import"
-end
+# File required to ensure cronjobs are removed
