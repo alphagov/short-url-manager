54706a3f6d5bdddd266366c2a440e498252d9231
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-10-11 15:05:48 +0100

    Update allowed hosts
    - update hosts list to match equivalent list in publishing API
    - add moneyhelper.org.uk as per https://govuk.zendesk.com/agent/tickets/5516679

--- app/models/concerns/short_url_validations.rb
+++ app/models/concerns/short_url_validations.rb
@@ -1,6 +1,19 @@
 module ShortUrlValidations
   extend ActiveSupport::Concern
 
+  # Ideally this should be kept up to date with the corresponding list in the publishing API
+  # https://github.com/alphagov/publishing-api/blob/main/app/validators/routes_and_redirects_validator.rb#L2-L10
+  EXTERNAL_HOST_ALLOW_LIST = %w[
+    .caa.co.uk
+    .gov.uk
+    .judiciary.uk
+    .moneyhelper.org.uk
+    .nationalhighways.co.uk
+    .nhs.uk
+    .police.uk
+    .ukri.org
+  ].freeze
+
   included do
     validates :from_path, :to_path, presence: true
     validates :from_path, format: { with: /\A\//, message: 'must be specified as a relative path (eg. "/hmrc/tax-returns")' }, allow_blank: true
@@ -9,14 +22,18 @@ module ShortUrlValidations
 
   def to_path_is_valid
     unless to_path.blank? || to_path =~ /\A\// || allowed_government_absolute_url?(to_path)
-      errors.add(:to_path, 'must be a relative path (eg. "/hmrc/tax-returns") or a government URL (eg. *.gov.uk, *.judiciary.uk, *.nhs.uk, *.ukri.org)')
+      errors.add(:to_path, "must be a relative path (eg. /hmrc/tax-returns) or a government URL (eg. #{ShortUrlValidations.allow_host_display_list})")
     end
   end
 
   def allowed_government_absolute_url?(path)
     uri = URI.parse(path)
-    uri.scheme == "https" && uri.host.end_with?(".gov.uk", ".judiciary.uk", ".nhs.uk", ".ukri.org") && uri.host != "www.gov.uk"
+    uri.scheme == "https" && uri.host.end_with?(*EXTERNAL_HOST_ALLOW_LIST) && uri.host != "www.gov.uk"
   rescue StandardError
     false
+  end
+
+  def self.allow_host_display_list
+    EXTERNAL_HOST_ALLOW_LIST.map { |host| "*#{host}" }.join(", ")
   end
 end
