8ab505cee8a9e7712785fe29473e26e0c2eabe39
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2024-03-01 10:49:05 +0000

    Configure action mailer to use GOV.UK Notify
    This configuration was lost during the upgrade to Rails 7.1. See 90eb0b9e9ff1da1e445aaeba242ea5efe5e7ae20

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "short_url_manager_production"
 
+  config.action_mailer.delivery_method = :notify
+  config.action_mailer.notify_settings = {
+    api_key: ENV["GOVUK_NOTIFY_API_KEY"],
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
